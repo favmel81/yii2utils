8bba48f6fdeb84c986f05b36a727a0a1c774c928
favmel81@gmail.com <favmel81@gmail.com>
2015-04-25 16:33:43 +0300

    Added web\Application class with resolveRoute event

---
+++ web/Application.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace yii2utils\web;
+
+use Yii;
+use yii\web\Application as WebApplication;
+use yii\base\InvalidRouteException;
+use yii\web\NotFoundHttpException;
+use yii\web\Response;
+
+class Application extends WebApplication {
+
+    const EVENT_RESOLVE_ROUTE = 'resolveRoute';
+
+    public function handleRequest($request)
+    {
+        if (empty($this->catchAll)) {
+            list ($route, $params) = $request->resolve();
+        } else {
+            $route = $this->catchAll[0];
+            $params = array_splice($this->catchAll, 1);
+        }
+
+        try {
+            Yii::trace("Route requested: '$route'", __METHOD__);
+            $this->requestedRoute = $route;
+            $this->trigger(self::EVENT_RESOLVE_ROUTE);
+            $result = $this->runAction($route, $params);
+            if ($result instanceof Response) {
+                return $result;
+            } else {
+                $response = $this->getResponse();
+                if ($result !== null) {
+                    $response->data = $result;
+                }
+
+                return $response;
+            }
+        } catch (InvalidRouteException $e) {
+            throw new NotFoundHttpException(Yii::t('yii', 'Page not found.'), $e->getCode(), $e);
+        }
+    }
+
+}
